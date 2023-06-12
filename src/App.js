25e14c05caec54f812fb42621408eda50b98bd87
Leejunho <wpsxmfwnsgh@gmail.com>
2023-06-12 13:06:58 +0900

    1.컴포넌트 기초

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
+import SayHello from './SayHello';
 
+
+
+
+//리액트에서는 하나의 컴포넌트가 여러개의 엘리먼트를 반환할 수 있습니다.
+//JSX를 작성할 때 return 문 안에 반드시 하나의 최상위 태그가 있어야 합니다.
+//Fragment를 사용하면 의미없는 태그를 줄여 가독성을 좋게 합니다. <> , </>
 function App() {
+
+  const looping = () => {
+    const helloList = [];
+
+    for(let i=0; i<5; i++){
+      helloList.push(<SayHello/>);
+    }
+    return helloList;
+  }
+
+  //jsx 문법에서는 스크립트를 코드로 직접 사용이 불가능.
+  //{} 안에서 함수의 호출문이나 변수참조는 가능.
   return (
-    <h1>리액트 초보입니다~~~</h1>
+    <>
+      { looping() }
+    </>
   );
 }
 
---
+++ src/SayHello.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//컴포넌트
+//컴포넌트 파일은 대문자로 작성하는 것이 관례입니다.
+function SayHello() {
+    return (
+
+        <div>
+            <p>hello</p>
+            <span>메롱</span>
+        </div>
+
+    );
+    
+}
+
+export default SayHello;
+
+
+
+
